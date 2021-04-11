48dece4ea7bf74910bc53def2cb6c9e65b5d0990
ValikMinak <valikminak@gmail.com>
2021-04-11 12:06:29 +0300

    Added recipe model

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 
-from core.models import User, Tag, Ingredient
+from core.models import User, Tag, Ingredient, Recipe
 
 
 @admin.register(User)
@@ -30,3 +30,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(Tag)
 admin.site.register(Ingredient)
+admin.site.register(Recipe)
